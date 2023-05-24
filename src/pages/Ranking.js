138e08a2fba50d0148f2840d15dff16ecdd9da09
Eduardo Klein <eduardo.klein@live.com>
2023-05-24 15:18:34 -0300

    Requisito 18 aprovado

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,13 +1,26 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
 class Ranking extends Component {
+  handleClick = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
   render() {
     return (
       <div>
         <h1 data-testid="ranking-title">Ranking</h1>
+        <button data-testid="btn-go-home" onClick={ this.handleClick }>Login</button>
       </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Ranking;
